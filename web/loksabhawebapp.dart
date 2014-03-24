b2a54a18376ca2257ea81e3e6d9bd55e9a251a3d
Sutikshan Dubey <s.anand.dubey@gmail.com>
2014-03-24 08:33:06 +0530

    -a

--- web/loksabhawebapp.dart
+++ web/loksabhawebapp.dart
@@ -1,9 +1,9 @@
 import 'dart:html' hide Point;
-import 'package:json/json.dart';
 import 'package:google_maps/google_maps.dart' as GoogleMaps;
-import 'dart:collection';
-GoogleMaps.GMap map;
+import 'model/loksabha.dart';
 
+GoogleMaps.GMap map;
+final LS2009Results ls2009 = new LS2009Results();
 final GoogleMaps.LatLng nagpur = new GoogleMaps.LatLng(21.21235, 79.10517);
 //final GoogleMaps.LatLng Lakshdweep = new GoogleMaps.LatLng(10.5700, 72.6300);
 var cname;
@@ -14,88 +14,52 @@ void main() {
     ..mapTypeId = GoogleMaps.MapTypeId.ROADMAP
     ;
   map = new GoogleMaps.GMap(querySelector("#map_canvas"), mapOptions);
-  loadConstWinners();
- loadContents(map);
+  ls2009.loadConstituencies(drawMap);
+  //loadContents();
+  //print ("success");
 }
 
 GoogleMaps.InfoWindow infoWindow = new GoogleMaps.InfoWindow();
-HashMap mpData = new HashMap();
-
-  void loadConstWinners(){
-    HttpRequest.getString('/sampleWebApp/data/winner2009.json').then((String jsonData){
-      final winnerData = parse (jsonData);
-      for (var mp in winnerData) {
-        mpData.putIfAbsent(mp["cty"],()=> mp);
-      }
-      loadContents(map);
-    });
-    }
-
-void loadContents(map){
-  HttpRequest.getString('/sampleWebApp/data/pc2009.json').then((String jsonData){
-  final data = parse (jsonData); 
-  var criminalCount = 0;
-  for (var pc in data["features"]) {
-    
-    if (pc["geometry"] != null){
-      
-      if (pc["geometry"]["coordinates"] != null && pc["geometry"]["coordinates"][0] != null){
-        
-        var constCoords = [];
-        for (var coord in pc["geometry"]["coordinates"][0]) {
-          
-          if (coord != null){
-            constCoords.add(new GoogleMaps.LatLng(coord[1], coord[0]));
-          }
-        }
-        // Construct the polygon.
-         var constPolygonOptions = new GoogleMaps.PolygonOptions()
-           ..paths=constCoords
-           ..strokeColor = '#FF0000'
-           ..strokeOpacity = 0.8
-           ..strokeWeight = 1
-           ..fillColor = '#00FF00'
-           ..fillOpacity = 0.35
-           ..clickable=true
-           ..map=map
-           
-         ;
-         var constProperties = pc["properties"];
-         var mp = mpData[constProperties["DPC_NM"]];
-       //  print (mp);
-         if (mp == null ){
-           constPolygonOptions.fillColor = '#00FF00';
-           print(constProperties["DPC_NM"]); //TODO explore these constituency name, these are not being find within ADR json.
-           if (constProperties["DPC_NM"] == null){
-             print ("dpc_nm was null, alternatives - ${constProperties}");
-           }
-         } else if ( mp["cc"] != null && mp["cc"] != "0"){
-           constPolygonOptions.fillColor = '#FF0000';
-           //print(mp["cc"]);
-           criminalCount = criminalCount+1;
-         } else {
-           constPolygonOptions.fillColor = '#00FF00';           
-         }
-         var constPolygon = new GoogleMaps.Polygon(constPolygonOptions);
-         constPolygon.onClick.listen((data){
-           print (constProperties);
-           print (mp);
-           cname = constProperties["MPC_NAME"];
-       
-        
-           infoWindow.content = contentString;
-           infoWindow.open(map, constPolygon);
-           //print(data);           
-         });
-      }
-     
-    }
-   
-  }
-  print(criminalCount);
-  });
+void drawMap(loksabha){
   
+  var criminalCount = 0;
+  // Construct the polygon.
+           var constPolygonOptions = new GoogleMaps.PolygonOptions()
+             ..paths=loksabha.Coordinates 
+             ..strokeColor = '#FF0000'
+             ..strokeOpacity = 0.8
+             ..strokeWeight = 1
+             ..fillColor = '#00FF00'
+             ..fillOpacity = 0.35
+             ..clickable=true
+             ..map=map           
+           ;
+           constPolygonOptions.fillColor = '#00FF00';
+           if (loksabha.ElectionData == null || loksabha.ElectionData.length == 0){
+             constPolygonOptions.fillColor = '#0000FF';  
+           }
+           else if (loksabha.ElectionData[0].wCrimeCases != null && int.parse(loksabha.ElectionData[0].wCrimeCases) > 0) {
+                constPolygonOptions.fillColor = '#FF0000';
+                criminalCount = criminalCount+1;
+              } else {
+                constPolygonOptions.fillColor = '#00FF00';           
+              }
+            var constPolygon = new GoogleMaps.Polygon(constPolygonOptions);
+            constPolygon.onClick.listen((data){
+             print(data);
+           
+              infoWindow.content = contentString;
+              infoWindow.open(map, constPolygon);
+              //print(data);        
+          });
 }
+void loadContents(){
+  for (var key in ls2009.allLokSabha.keys){
+    drawMap(key);  
+  }
+  
+  
+      }
 var contentString = """<div id="content">
       <div id="siteNotice">
       </div>
--- web/model/loksabha.dart
+++ web/model/loksabha.dart
@@ -1,121 +1,125 @@
 import 'dart:html' hide Point;
 import 'package:json/json.dart';
 import 'dart:collection';
+import 'package:google_maps/google_maps.dart' as GoogleMaps;
 
-class Loksabha {
+class LokSabha {
+  LokSabha(this.DPC_ID);   
+  String get Id => DPC_ID;
+  String get Name => DPC_NM;
+  String get StateId => DB_STATEID;
+  String get StateName => DSTATE_NM;
+  String get LSDetailUrl => "http://www.indiavotes.com/pc/detail/$DPC_ID/$DB_STATEID/15";
+  
+  List<GoogleMaps.LatLng> Coordinates;
+  List<LSElectionResult> ElectionData;
+  
   var DPC_ID;
   var DPC_NM;
-
-  num DB_STATEID;
+  var DB_STATEID;
   var DSTATE_NM;
-
-  String get LSDetailUrl => "http://www.indiavotes.com/pc/detail/$DPC_ID/$DB_STATEID/15";
-
-  List<Coordinate> Coordinates;
-  List<LSElectionResult> ElectionData;
-}
-class Coordinate {
-  num Lat;
-  num Long;
-}
-
-class LSElectionResult {
-  num DPC_ID; // matches with MPC_NO of Loksabha constituency
-  var DWIN_NM; //winner name
-  var DWIN_PRT; // winner party
   
+  Map toJson() {
+     return {"id": DPC_ID, "name": DPC_NM, "stateId": DB_STATEID, "stateName": DSTATE_NM, "ElecData2009": ElectionData};
+   }
+}
+class LSElectionResult {  
   
   String get ElectionType => "Loksabha";
   String get ElectionYear => "2009";
-  num get ElectionId      =>  15; //matches the indiavotes id - 15 for Loksabha 15 election. 
+  num get ElectionId      =>  15; //matches the indiavotes id - 15 for Loksabha 15 election.
   
-  var WinnerCandidateId; // match the myneta info id
-  String get MPDetailUrl => "http://www.myneta.info/ls2009/candidate.php?candidate_id=$WinnerCandidateId";
-  
+  String get MPDetailUrl => "http://www.myneta.info/ls2009/candidate.php?candidate_id=$WADRId";  
   String get WinnerName => DWIN_NM;
   String get WinnerParty => DWIN_PRT;
+  
   var RunnerUp;
-  var RunnerUpParty;
-  var TotalVote;
-  var WinnerVoteCount;
-  var RunnerUpVoteCount;
+  var RUParty;  
+  var WVCount;
+  var RUVCount;
+  var totalV;
+  var VMargin;
+  var DPC_ID; // matches with DPC_ID of Loksabha constituency
+  var DWIN_NM; //winner name
+  var DWIN_PRT; // winner party
+
+  //ADR Data    
+  var WADRId; // match the myneta info id
+  var WEdu;
+  var WAssets;
+  var WLia;
+  var wCrimeCases;
+
 }
 class LS2009Results {
-  HashMap mpData = new HashMap();
   
-  void loadConstWinners(){
-    HttpRequest.getString('/sampleWebApp/data/winner2009.json').then((String jsonData){
-      final winnerData = parse (jsonData);
-      for (var mp in winnerData) {
-        mpData.putIfAbsent(mp["cty"],()=> mp);
-      }
-      loadContents(map);
-    });
-    }
-
-void loadContents(map){
-  HttpRequest.getString('/sampleWebApp/data/pc2009.json').then((String jsonData){
-  final data = parse (jsonData); 
-  var criminalCount = 0;
-  for (var pc in data["features"]) {
-    
-    if (pc["geometry"] != null){
-      
-      if (pc["geometry"]["coordinates"] != null && pc["geometry"]["coordinates"][0] != null){
-        
-        var constCoords = [];
-        for (var coord in pc["geometry"]["coordinates"][0]) {
-          
-          if (coord != null){
-            constCoords.add(new GoogleMaps.LatLng(coord[1], coord[0]));
-          }
+  HashMap<String, LokSabha> allLokSabha = new HashMap<String, LokSabha>();
+  
+  LokSabha loadLokSabhaData(var mp, var pcGeometry) {
+    LokSabha lokSabha = new LokSabha(int.parse(mp["DPC_ID"]));
+    lokSabha.DPC_NM = mp["DPC_NM"];
+    lokSabha.DB_STATEID = int.parse(mp["DB_STATEID"]);
+    lokSabha.DSTATE_NM = mp["DSTATE_NM"];
+    lokSabha.Coordinates = new List<GoogleMaps.LatLng>();
+    if (pcGeometry!= null && pcGeometry["coordinates"] != null && pcGeometry["coordinates"].length > 0) {
+      for (var coord in pcGeometry["coordinates"][0]) {            
+        if (coord != null) {
+          lokSabha.Coordinates.add(new GoogleMaps.LatLng(coord[1], coord[0]));
         }
-        // Construct the polygon.
-         var constPolygonOptions = new GoogleMaps.PolygonOptions()
-           ..paths=constCoords
-           ..strokeColor = '#FF0000'
-           ..strokeOpacity = 0.8
-           ..strokeWeight = 1
-           ..fillColor = '#00FF00'
-           ..fillOpacity = 0.35
-           ..clickable=true
-           ..map=map
-           
-         ;
-         var constProperties = pc["properties"];
-         var mp = mpData[constProperties["DPC_NM"]];
-       //  print (mp);
-         if (mp == null ){
-           constPolygonOptions.fillColor = '#00FF00';
-           print(constProperties["DPC_NM"]); //TODO explore these constituency name, these are not being find within ADR json.
-           if (constProperties["DPC_NM"] == null){
-             print ("dpc_nm was null, alternatives - ${constProperties}");
-           }
-         } else if ( mp["cc"] != null && mp["cc"] != "0"){
-           constPolygonOptions.fillColor = '#FF0000';
-           //print(mp["cc"]);
-           criminalCount = criminalCount+1;
-         } else {
-           constPolygonOptions.fillColor = '#00FF00';           
-         }
-         var constPolygon = new GoogleMaps.Polygon(constPolygonOptions);
-         constPolygon.onClick.listen((data){
-           print (constProperties);
-           print (mp);
-           cname = constProperties["MPC_NAME"];
-       
-        
-           infoWindow.content = contentString;
-           infoWindow.open(map, constPolygon);
-           //print(data);           
-         });
       }
-     
     }
-   
+    lokSabha.ElectionData = new List<LSElectionResult> ();
+    LSElectionResult ls2009 = new LSElectionResult();
+    lokSabha.ElectionData.add(ls2009);
+    ls2009.DPC_ID = lokSabha.DPC_ID;
+    ls2009.DWIN_NM = mp["DWIN_NM"];
+    ls2009.DWIN_PRT = mp["DWIN_PRT"];
+    ls2009.RunnerUp = mp["DRU_CND"];
+    ls2009.RUParty = mp["DRU_PRT"];
+    
+    ls2009.RUVCount = mp["DRU_VOT"];
+    ls2009.WVCount = mp["DCND_V"];
+    
+    ls2009.totalV = mp["DTOT_VOT"];
+    ls2009.VMargin = num.parse(mp["DMAR_P"]);
+    return lokSabha;
   }
-  print(criminalCount);
-  });
   
-}
+  void loadConstituencies(drawMap){
+    HttpRequest.getString('/loksabhaWebApp/data/pc2009.json').then((String jsonData){
+      final data = parse (jsonData);
+      for (var pc in data["features"]) {
+        var pcProperties = pc["properties"];
+        var pcGeometry = pc["geometry"];
+        
+        if (pcProperties["DPC_NM"] != null && pcProperties["DPC_NM"] != "") {
+                allLokSabha.putIfAbsent(pcProperties["DPC_NM"], () => loadLokSabhaData(pcProperties, pcGeometry));
+        }
+      }
+        loadADRData(drawMap);
+      
+    });
+  }
+
+  void loadADRData(drawMap){
+  HttpRequest.getString('/loksabhaWebApp/data/winner2009.json').then((String jsonData){
+    final winnerData = parse (jsonData);
+        for (var mp in winnerData) {
+          LokSabha ls = allLokSabha[mp["cty"]];
+          if (ls != null) {
+            LSElectionResult lsElectionResult =  ls.ElectionData[0];
+            lsElectionResult.WADRId = mp["id"];
+            lsElectionResult.WAssets = mp["a"];
+            lsElectionResult.WLia = mp["l"];
+            lsElectionResult.wCrimeCases = mp["cc"];
+            lsElectionResult.WEdu = mp["e"];            
+            
+          } else {
+            // detail not found in ADR site.
+            print (mp["cty"] + "--> Detail not found in indiavotes.in");
+          }
+          drawMap(ls);
+        } 
+  });  
+  }
 }
